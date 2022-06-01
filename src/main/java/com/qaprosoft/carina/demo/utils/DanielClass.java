9c1440c5c503ee599b711d67b35308a6dfb1c0a5
DanielGRodriguez <drodriguez@gmail.com>
2022-06-01 10:33:14 -0300

    Daniel class change

--- src/main/java/com/qaprosoft/carina/demo/utils/DanielClass.java
+++ src/main/java/com/qaprosoft/carina/demo/utils/DanielClass.java
@@ -3,5 +3,6 @@ package com.qaprosoft.carina.demo.utils;
 public class DanielClass {
     public void isActive() {
         System.out.println("I exist");
+        System.out.println("I still exist");
     }
 }
